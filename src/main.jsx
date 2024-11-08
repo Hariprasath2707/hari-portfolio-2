808adef32988e89bdf6c88ab7bf3edc1802de4a3
Hariprasath2707 <hariprasath642@gamil.com>
2024-11-08 22:45:52 +0530

    fixing

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { ThemeProvider } from './common/themeContext.jsx';
+import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider>
+
+  <BrowserRouter basename="hari-portfolio-2">
+  <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/vite-react-deploy/",
+  plugins: [react()], // Adding React plugin for Vite
+  base: "hari-portfolio-2", // Base path for deployment (used for GitHub Pages)
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'] // Excluding 'react-router-dom' from the bundle
+    }
+  }
 })
+
